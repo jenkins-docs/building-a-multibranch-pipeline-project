374d42539e2e1b6b38cb95bc8e99c318fcb2f5e8
Giles Gaskell <gilesgas@gmail.com>
2017-11-16 16:35:22 +1100

    Update scripts and Jenkinsfile.

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
-set -x
-npm run build
-set +x
-
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
