4c873c39241d1465bb1191a4a421b5e8dab200e9
Giles Gaskell <gilesgas@gmail.com>
2017-11-21 23:01:38 +1100

    Update 'kill.sh' script description.

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
+echo '(written to ".pidfile"), all of which were conducted when either'
+echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 set -x
 kill $(cat .pidfile)
