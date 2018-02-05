74b87c1057a6e29cb59766ed26568a587b415db4
dumln86 <dumln86@gmail.com>
2018-02-06 00:12:30 +0700

    update kill.sh4

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
-
+##!/usr/bin/env sh
+#!/bin/bash
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when either'
 echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
-set -x
+#set -x
 #kill $(cat .pidfile)
-sudo ssh -i /home/.ssh/jenkins_rsa root@35.194.253.9 'mkdir /home/GitHub'
+ssh -i /home/.ssh/jenkins_rsa root@35.194.253.9 'mkdir /home/GitHub'
