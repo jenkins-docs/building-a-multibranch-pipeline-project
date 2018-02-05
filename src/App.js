27b53f8b155a33fe56f07d0e8c7ef49d2d92e8dd
dumln86 <dumln86@gmail.com>
2018-02-05 09:33:38 +0700

    Ap.js 05052018

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Toooooooooooo getttttttttttttttttttttttt started, edit <code>src/App.js</code> and save to reload.
+          Toaaiaaaaaaaaaaaaaaaaooooooooooo getttttttttttttttttttttttt started, edit <code>src/App.js</code> and save to reload.
 	<br/>
           This is a new line I added.
         </p>
