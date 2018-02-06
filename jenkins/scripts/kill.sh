e5805f58b9ae06f33d24cf25bf72caf50c47f44f
dumln86 <dumln86@gmail.com>
2018-02-06 09:55:28 +0700

    killsh askdds

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,6 +1,6 @@
 ##!/usr/bin/env sh
 #!/bin/bash
-echo 'The following command terminates the "npm start" process using its PID'
+echo 'TTTTTTTTTTTTTTTTTTTTTTTTTTThe following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when either'
 echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 #set -x
