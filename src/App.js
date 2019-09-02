235a6ca4a8deabc95f581f9bbf9cbe1232907276
Giri <544270@cognizant.com>
2019-09-02 11:45:17 +0530

    Merge request

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           <br/>
-          This is new line I added.
+          This is new line I added in development.
+		  <br/>
+		  Testing the pull request.
         </p>
       </div>
     );
