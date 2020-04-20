d3481835924d609bb89812a7dea6d5223ef3b88a
MastanM <41544446+MastanM@users.noreply.github.com>
2020-04-20 16:33:39 +0530

    added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>
+          This is a new line I added.
         </p>
       </div>
     );
