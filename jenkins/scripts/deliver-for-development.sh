4ac3846318ebbb5c8965abada3756f0a3fd3a4e7
Iqlas15 <35040151+Iqlas15@users.noreply.github.com>
2020-06-03 19:00:23 +0530

    Update deliver-for-development.sh

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -15,6 +15,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit http://192.168.99.15:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
