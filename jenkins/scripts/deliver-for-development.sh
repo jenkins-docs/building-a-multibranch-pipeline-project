4e47f57790351af4d2fcef851bb0ea47a74235df
Nicola <nscuor@units.it>
2024-02-13 16:44:32 +0100

    dev

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -8,6 +8,7 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
+
 set -x
 npm start &
 echo $! > .pidfile
