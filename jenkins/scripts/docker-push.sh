43ad5c961830894b11213d53f975945d0fb67dea
nik71b14 <nicola@goinnovation.eu>
2024-02-15 18:16:35 +0100

    Update docker-push.sh

--- jenkins/scripts/docker-push.sh
+++ jenkins/scripts/docker-push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "$DOCKERHUB_CREDENTIALS_PSW"
-#echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
+echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
 #docker push USER/REPO
