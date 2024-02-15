52cdac56cb9f0d60c9edaa053a24aeb8f08f5994
nik71b14 <nicola@goinnovation.eu>
2024-02-15 18:37:47 +0100

    Update docker-push.sh

--- jenkins/scripts/docker-push.sh
+++ jenkins/scripts/docker-push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
-sudo docker push "test-image:$BUILD_ID"
+sudo docker push "goinnova/test-image:$BUILD_ID"
