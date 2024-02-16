7d17833890df85212004214c6eba75a9ec1acfa8
Nicola <nscuor@units.it>
2024-02-16 09:26:17 +0100

    docker-push

--- jenkins/scripts/docker-push.sh
+++ jenkins/scripts/docker-push.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Login
-echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
+#echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
+echo $DOCKERHUB_CREDENTIALS_PSW | sudo docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
 
 # Cambia nome - NB: l'immagine base deve essere già presente in docker.io!!!
 sudo docker tag "test-image:$BUILD_ID" "goinnova/test-image:$BUILD_ID"
