ddac953f7c9709eff9cf0d41f7410b9bf9c64706
Nicola <nscuor@units.it>
2024-02-20 17:37:44 +0100

    Modificato docker-push.sh

--- jenkins/scripts/docker-push.sh
+++ jenkins/scripts/docker-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Login
-#echo "$DOCKERHUB_CREDENTIALS_PSW" | sudo docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
+# https://docs.docker.com/engine/reference/commandline/login/#credentials-store
 echo $DOCKERHUB_CREDENTIALS_PSW | sudo docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
 
 # Cambia nome - NB: l'immagine base deve essere già presente in docker.io!!!
