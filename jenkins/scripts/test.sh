86591217456052bd76b62bacc6da4f055de9707b
Mohammed Alqabbani <malqabbani@circlys.com>
2024-07-09 14:37:36 +0300

    test

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash sh
 
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
