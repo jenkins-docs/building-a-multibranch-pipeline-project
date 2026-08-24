40016cd94858fd7f7674a4e7ae08e33bfde555c6
tiriths <brian.capes@cox.net>
2026-08-24 14:32:47 -0700

    Restore the test

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toHaveTextContent("someFalseValue");
-  expect(true).toBe(false);
+  //intentionally break the test
+  //expect(linkElement).toHaveTextContent("someFalseValue");
+  //expect(true).toBe(false);
 });
