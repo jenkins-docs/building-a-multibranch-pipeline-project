f6c094206ab30eab6b0b619c06a13b2025191870
tiriths <brian.capes@cox.net>
2026-08-24 14:26:04 -0700

    Intentionally break test for Actions exercise: Take 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  !expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toHaveTextContent("someFalseValue");
+  expect(true).toBe(false);
 });
